fede60494d69bb27bfb749be4143105d46fe3c22
13ruceYu <bruceyuhongbo@gmail.com>
2022-09-27 23:26:27 +0800

    feat: plum finish

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,89 @@
 const el = $ref<HTMLCanvasElement>()
 const ctx = $computed(() => el!.getContext('2d')!)
 
-function init() {
-  ctx.strokeStyle = '#fff'
-  line(0, 0, 30, 30)
+const WIDTH = 600
+const HEIGHT = 600
+
+interface Point {
+  x: number
+  y: number
 }
 
-function line(x1: number, y1: number, x2: number, y2: number) {
+interface Branch {
+  start: Point
+  length: number
+  theta: number
+}
+
+function init() {
+  ctx.strokeStyle = '#fff'
+
+  const branch = {
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 40,
+    theta: -Math.PI / 2,
+  }
+
+  step(branch)
+}
+
+const pendingTasks: Function[] = []
+
+function step(b: Branch, depth = 0) {
+  const end = getEndPoint(b)
+  drawBranch(b)
+
+  if (depth < 2 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta - 0.3 * Math.random(),
+    }, depth + 1))
+  }
+
+  if (depth < 2 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta + 0.3 * Math.random(),
+    }, depth + 1))
+  }
+}
+
+function frame() {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+
+let frameCount = 0
+function startFrame() {
+  requestAnimationFrame(() => {
+    frameCount += 1
+    if (frameCount % 3 === 0)
+      frame()
+    startFrame()
+  })
+}
+
+startFrame()
+
+function lineTo(p1: Point, p2: Point) {
   ctx.beginPath()
-  ctx.moveTo(x1, y1)
-  ctx.lineTo(x2, y2)
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
   ctx.stroke()
+}
+
+function getEndPoint(b: Branch): Point {
+  return {
+    x: b.start.x + b.length * Math.cos(b.theta),
+    y: b.start.y + b.length * Math.sin(b.theta),
+  }
+}
+
+function drawBranch(b: Branch) {
+  lineTo(b.start, getEndPoint(b))
 }
 
 onMounted(() => {
@@ -20,5 +93,5 @@ onMounted(() => {
 </script>
 
 <template>
-  <canvas ref="el" width="400" height="400" border />
+  <canvas ref="el" width="600" height="600" border />
 </template>
